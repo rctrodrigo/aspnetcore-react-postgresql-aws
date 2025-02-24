8e96411d93795c66a369db4dd386d79f73a27fed
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-02-24 19:20:05 +0800

    feat(mode-toggle): add dark mode and system theme support

--- portfolio-ui/src/app/index.tsx
+++ portfolio-ui/src/app/index.tsx
@@ -1,10 +1,13 @@
 import Footer from "@/components/layouts/footer.tsx";
+import {ThemeProvider} from "@/features/theme-provider.tsx";
+import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
 
 export const App = () => {
     return (
-        <>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ModeToggle />
             <Footer />
-        </>
+        </ThemeProvider>
     );
 };
 
--- portfolio-ui/src/components/layouts/footer.tsx
+++ portfolio-ui/src/components/layouts/footer.tsx
@@ -1,8 +1,8 @@
-const copyrightYear = new Date().getFullYear();
+const copyrightYear: number = new Date().getFullYear();
 
 export default function Footer() {
     return (
-        <footer className="w-full fixed bottom-0 mb-2 text-center">
+        <footer className="w-full fixed bottom-0 mb-2 text-center dark:bg-gray-800 dark:text-amber-50">
                 &copy; {copyrightYear} Raven Rodrigo
         </footer>
     );
