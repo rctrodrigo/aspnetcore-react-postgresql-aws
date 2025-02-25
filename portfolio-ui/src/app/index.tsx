c0a5f876549c1a1fab6b1dc0d62d5a38a476d720
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-02-25 18:27:56 +0800

    feat(about-section): add avatar and svg icons

--- portfolio-ui/src/app/index.tsx
+++ portfolio-ui/src/app/index.tsx
@@ -2,12 +2,14 @@ import Footer from "@/components/layouts/footer.tsx";
 import {ThemeProvider} from "@/features/theme-provider.tsx";
 import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
 import Navbar from "@/components/layouts/navbar.tsx";
+import About from "@/components/layouts/about.tsx";
 
 export const App = () => {
     return (
         <>
             <div className="grid grid-cols-4 grid-rows-6 gap-9">
                 <Navbar />
+                <About />
             </div>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                 <ModeToggle />
