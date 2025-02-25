17ce92a4e296235f767759dbb3ea4a80bce10b8c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-02-25 19:52:33 +0800

    feat(tools-section): add tools svg icons

--- portfolio-ui/src/app/index.tsx
+++ portfolio-ui/src/app/index.tsx
@@ -3,13 +3,15 @@ import {ThemeProvider} from "@/features/theme-provider.tsx";
 import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
 import Navbar from "@/components/layouts/navbar.tsx";
 import About from "@/components/layouts/about.tsx";
+import Tools from "@/components/layouts/tools.tsx";
 
 export const App = () => {
     return (
         <>
-            <div className="grid grid-cols-4 grid-rows-6 gap-9">
+            <div className="grid grid-cols-4 grid-rows-6 gap-4">
                 <Navbar />
                 <About />
+                <Tools />
             </div>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                 <ModeToggle />
