bf6341665d141c21e7070432fa73b0117353b662
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-02-25 16:07:04 +0800

    feat(navbar): add site name link and navigation links

---
+++ portfolio-ui/src/app/content.ts
@@ -0,0 +1,11 @@
+export const siteName: string = "Raven Rodrigo";
+export const navigationLinks: string[] = [
+    "Home",
+    "About",
+    "Blog",
+    "Projects",
+    "Tools",
+    "Contact"
+];
+
+
--- portfolio-ui/src/app/index.tsx
+++ portfolio-ui/src/app/index.tsx
@@ -1,13 +1,19 @@
 import Footer from "@/components/layouts/footer.tsx";
 import {ThemeProvider} from "@/features/theme-provider.tsx";
 import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
+import Navbar from "@/components/layouts/navbar.tsx";
 
 export const App = () => {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <ModeToggle />
-            <Footer />
-        </ThemeProvider>
+        <>
+            <div className="grid grid-cols-4 grid-rows-6 gap-9">
+                <Navbar />
+            </div>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <ModeToggle />
+                <Footer />
+            </ThemeProvider>
+        </>
     );
 };
 
